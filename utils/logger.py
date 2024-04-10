fc2bfbdfbdfb238d175c57a6e51c55a6adad612d
Uzkiy Dmitriy <fallowthestreet@yandex.ru>
2024-04-10 23:05:30 +0300

    add some shit

---
+++ utils/logger.py
@@ -0,0 +1,11 @@
+import logging 
+
+
+def setup_logger(level: int = logging.INFO) -> None:
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s | %(name)s: %(message)s",
+        datefmt="[%H:%M:%S]",
+        level=level,
+    )
+    return logger
